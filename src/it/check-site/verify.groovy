2310e861c0ec945ee384efe72969a6070c71b13c
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2012-05-10 23:08:55 +0200

    Add integration test for site reports

---
+++ src/it/check-site/verify.groovy
@@ -0,0 +1,24 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+assert new File( basedir, "target/cpd.xml" ).exists();
+assert new File( basedir, "target/site/cpd.html" ).exists();
+
+assert new File( basedir, "target/pmd.xml" ).exists();
+assert new File( basedir, "target/site/pmd.html" ).exists();
+
+assert new File( basedir, "target/findbugs.xml" ).exists();
+assert new File( basedir, "target/site/findbugs.html" ).exists();
+
+assert new File( basedir, "target/checkstyle-result.xml" ).exists();
+assert new File( basedir, "target/site/checkstyle.html" ).exists();
+
+assert new File( basedir, "target/site/cobertura/index.html" ).exists();
+assert new File( basedir, "target/site/cobertura/coverage.xml" ).exists();
+
+assert new File( basedir, "target/site/xref/index.html" ).exists();
+
+assert new File( basedir, "target/apidocs/index.html" ).exists();
+assert new File( basedir, "target/site/apidocs/index.html" ).exists();
+
+return true;
