6ec29ff8bbcdbf511a7db4509b00ce471d3fcdf2
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2013-01-23 22:19:46 +0100

    Fixes #20: Enable execution of jacoco:report by default for coverage report in Netbeans.
    * Include report for jacoco in prepare.
    * Add integration test for a pom only project (so no jacoco report is rendered).
    * All integration tests run successully.

---
+++ src/it/check-pom-jacoco/verify.groovy
@@ -0,0 +1,9 @@
+File file = new File( basedir, "build.log" );
+assert file.exists();
+
+String buildLog = file.getText("UTF-8");
+assert !buildLog.contains('org.apache.maven.project.ProjectBuildingException');
+
+assert !buildLog.contains('net.sourceforge.cobertura.reporting.xml.XMLReport')
+
+return true;
