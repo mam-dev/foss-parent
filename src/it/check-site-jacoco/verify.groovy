388ad933b2168557063970d2ec37e5fe72f8189d
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2015-10-31 20:45:58 +0100

    github-report is not created when running in travis

--- src/it/check-site-jacoco/verify.groovy
+++ src/it/check-site-jacoco/verify.groovy
@@ -32,7 +32,9 @@ assert new File( basedir, "target/site/xref/index.html" ).exists();
 assert new File( basedir, "target/apidocs/index.html" ).exists();
 assert new File( basedir, "target/site/apidocs/index.html" ).exists();
 
-assert new File( basedir, "target/site/github-report.html").exists();
+if (System.getenv('CONTINUOUS_INTEGRATION') == null) {
+    assert new File( basedir, "target/site/github-report.html").exists();
+}
 
 File[] files = new File( basedir, "target/repo/net/oneandone/maven/poms/sample-project-jacoco/3000-SNAPSHOT/").listFiles();
 assert files.toString().findAll("sample-project-jacoco-3000-.*-sources\\.jar").size()>0;
