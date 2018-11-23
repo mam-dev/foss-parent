e566efe36c0926550bc9203eb8b7f98b51161576
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2018-11-23 18:12:54 +0100

    Include invoker test for spring-boot #112
    * We need to manage the versions of logback and slf4j back to
      released versions, though.

--- src/it/check-pom-jacoco/verify.groovy
+++
@@ -1,9 +0,0 @@
-File file = new File( basedir, "build.log" );
-assert file.exists();
-
-String buildLog = file.getText("UTF-8");
-//assert !buildLog.contains('org.apache.maven.project.ProjectBuildingException');
-
-assert !buildLog.contains('net.sourceforge.cobertura.reporting.xml.XMLReport')
-
-return true;
