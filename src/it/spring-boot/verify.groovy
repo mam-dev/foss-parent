267945e49677bb88dd7a70865f886af95a503928
Mirko Friedenhagen <mirko.friedenhagen@1und1.de>
2018-11-23 18:50:53 +0100

    Include support for spock tests #112

--- src/it/spring-boot/verify.groovy
+++ src/it/spring-boot/verify.groovy
@@ -1,24 +1,24 @@
 // File basedir = new File( "." );
 File file = new File(basedir, "build.log")
+File targetDir = new File(basedir, "target")
+
 assert file.exists()
+assert targetDir.exists()
 
-String buildLog = file.getText("UTF-8")
+[
+    'failsafe-reports/failsafe-summary.xml',
+    'failsafe-reports/failsafe-summary.xml',
+    'cpd.xml',
+    'pmd.xml',
+    'spotbugsXml.xml',
+    'checkstyle-result.xml',
+    'jacoco.exec',
+    'surefire-reports/TEST-net.oneandone.maven.poms.sampleprojectspringboot.SpockTest.xml'
+].forEach {
+    assert new File(targetDir, it).exists()
+}
 
-assert !buildLog.contains('net.sourceforge.cobertura.reporting.xml.XMLReport')
-
-assert new File(basedir, "target/failsafe-reports/failsafe-summary.xml").exists()
-
-assert new File(basedir, "target/cpd.xml").exists()
-
-assert new File(basedir, "target/pmd.xml").exists()
-
-assert new File(basedir, "target/spotbugsXml.xml").exists()
-
-assert new File(basedir, "target/checkstyle-result.xml").exists()
-
-assert new File(basedir, "target/jacoco.exec").exists()
-
-File[] files = new File(basedir, "target/").listFiles()
+File[] files = targetDir.listFiles()
 assert files.toString().findAll("sample-project-spring-boot-3000-SNAPSHOT-sources\\.jar").size() > 0
 assert files.toString().findAll("sample-project-spring-boot-3000-SNAPSHOT\\.jar").size() > files.toString().findAll("sample-project-spring-boot-3000-SNAPSHOT\\.jar\\.original").size()
 return true
